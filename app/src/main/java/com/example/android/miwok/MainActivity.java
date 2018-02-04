f8fee635e31380ba2a89202fc2ca40e5ec2b35a5
Zizoh Anto <zizohanto@hotmail.com>
2018-02-04 17:06:26 +0100

    create words array in NumbersActivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
             // The code in this method will be executed when the numbers View is clicked on.
             @Override
             public void onClick(View view) {
-               Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-               startActivity(numbersIntent);
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
             }
         });
 
