7945efaea1992d024a59266c2bf21b932941b008
Zizoh Anto <zizohanto@hotmail.com>
2018-02-04 21:34:07 +0100

    modify while loop to for loop to create and display TextView for each element in the words list

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -29,13 +29,15 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        // while loop to create and display TextView for each element in the words list
-        int index = 0;
-        while (index < words.size()) {
+        // for loop to create and display TextView for each element in the words list
+        // Create a variable to keep track of the current index position
+        for (int index = 0; index < words.size(); index++) {
+            // Create a new TextView to display the word for the element
+            // Get the word for the element and sets it to the TextView created
+            // Add the created TextView to the parent rootView
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             rootView.addView(wordView);
-            index++;
         }
     }
 }
