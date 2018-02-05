e6bd94a49da3006d0c2da399e27d8a01dcfca2cd
Zizoh Anto <zizohanto@hotmail.com>
2018-02-05 21:48:35 +0100

    create custom Word class in Word.java

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,49 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Zizoh on 05/02/2018.
+ */
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
