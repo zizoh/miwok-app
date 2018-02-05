c83faf1a2aab3b007ec3111bea0eca067f9dc182
Zizoh Anto <zizohanto@hotmail.com>
2018-02-05 23:53:29 +0100

    modify FamilyActivity by adding the Miwok and English translations of family members

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,13 +1,45 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.activity_numbers);
+
+        // Create an arraylist of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@Word}s. The
+        // adapter knows how to create list items in the list
+        WordAdapter adapter =
+                new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} created above so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument which is the {@link WordAdapter} with the variable name adapter.
+        listView.setAdapter(adapter);
     }
 }
